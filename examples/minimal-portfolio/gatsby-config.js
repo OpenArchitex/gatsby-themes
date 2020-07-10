32ba765abe640dc7b286c07b0f0e512c686da0e7
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-07-10 08:53:32 -0700

    fix: favicon problem with svg image
    for some reason the automatic favicon generation in gatsby seems to have problems with svg files so we will switch to png for favicon

--- examples/minimal-portfolio/gatsby-config.js
+++ examples/minimal-portfolio/gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
                 background_color: `#f7f0eb`,
                 theme_color: `#a2466c`,
                 display: `standalone`,
-                icon: `src/images/favicon.svg`,
+                icon: `src/images/favicon.png`,
             },
         },
         `gatsby-plugin-offline`,
